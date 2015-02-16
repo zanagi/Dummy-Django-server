3017edc1367a4bb9c3b2d4971609942c1223673d
zanagi <shany1@niksula.hut.fi>
2015-02-16 16:00:38 +0200

    setting fixes

--- shorturl/settings.py
+++ shorturl/settings.py
@@ -99,9 +99,8 @@ TEMPLATE_DIRS = (
     './shorturl/templates',
 )
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Enable Connection Pooling
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
